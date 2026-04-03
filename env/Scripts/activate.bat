fc1beffba9d4dcc9027b96f1d58b2eff303a8c13
Midori-Midori <mj.aguilar2003@gmail.com>
2026-04-02 20:12:39 -0700

    actualizacion de mi rama

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\FELIPE QUIROZ\Source\Repos\proyecto_mexicali\env
+set VIRTUAL_ENV=C:\Users\majo\OneDrive\Escritorio\proyecto_mexicali\env
 
 if not defined PROMPT set PROMPT=$P$G
 
